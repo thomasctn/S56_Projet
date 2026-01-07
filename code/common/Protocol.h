a38e65d557d2aa003b1cf1724b79b1c01d43a186
BetaNotix <benx2005@outlook.fr>
2026-01-07 15:30:17 +0000

    Ajout "ServerListRoomPlayers" dans Protocol.h

--- code/common/Protocol.h
+++ code/common/Protocol.h
@@ -8,7 +8,6 @@
 
 using namespace gf::literals;
 
-
 /*
 Liste des échanges client <-> serveur
 
@@ -44,165 +43,193 @@ Serveur (envoyé)
 
 
 */
-struct PlayerData {
-    static constexpr gf::Id type = "PlayerData"_id;
-    uint32_t id;
-    float x, y;
-    uint32_t color; // RGBA
-    std::string name;
-    PlayerRole role;
-    int score;
-    bool ready = false;
+struct PlayerData
+{
+  static constexpr gf::Id type = "PlayerData"_id;
+  uint32_t id;
+  float x, y;
+  uint32_t color; // RGBA
+  std::string name;
+  PlayerRole role;
+  int score;
+  bool ready = false;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, PlayerData& data) {
-    return ar | data.id | data.x | data.y | data.color | data.name | data.role | data.score| data.ready;
-}
-struct GameSettigns {
-    static constexpr gf::Id type = "GameSettigns"_id;
-    int duration = 0;
-    std::vector<PlayerData> players;
-};
-template<typename Archive>
-  Archive& operator|(Archive& ar, GameSettigns& data) {
-    return ar | data.duration | data.players;
+template <typename Archive>
+Archive &operator|(Archive &ar, PlayerData &data)
+{
+  return ar | data.id | data.x | data.y | data.color | data.name | data.role | data.score | data.ready;
 }
 
+// Un peu inspiré de https://github.com/Hatunruna/ggj2020/blob/master/code/bits/common/Protocol.h
 
+// Serveur -> client
 
-//Un peu inspiré de https://github.com/Hatunruna/ggj2020/blob/master/code/bits/common/Protocol.h
-
-
-//Serveur -> client
-
-struct ServerJoinRoom {
-    static constexpr gf::Id type = "ServerJoinRoom"_id;
+struct ServerJoinRoom
+{
+  static constexpr gf::Id type = "ServerJoinRoom"_id;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, ServerJoinRoom& data) {
-    return ar;
+template <typename Archive>
+Archive &operator|(Archive &ar, ServerJoinRoom &data)
+{
+  return ar;
 }
-struct ServerLeaveRoom {
-    static constexpr gf::Id type = "ServerLeaveRoom"_id;
+struct ServerLeaveRoom
+{
+  static constexpr gf::Id type = "ServerLeaveRoom"_id;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, ServerLeaveRoom& data) {
-    return ar;
+template <typename Archive>
+Archive &operator|(Archive &ar, ServerLeaveRoom &data)
+{
+  return ar;
 }
-
-struct ServerChangeRole {
-    static constexpr gf::Id type = "ServerChangeRole"_id;
+//Utilisé pour mettre à jour le nombre de joueur, le changement de rôle de joueur, le changement de couleur, etc...
+struct ServerListRoomPlayers
+{
+  static constexpr gf::Id type = "ServerListRoomPlayers"_id;
+  std::vector<PlayerData> players;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, ServerChangeRole& data) {
-    return ar;
+template <typename Archive>
+Archive &operator|(Archive &ar, ServerListRoomPlayers &data)
+{
+  return ar | data.players;
 }
-
-//Les autres changement
-
-struct ServerReady {
-    static constexpr gf::Id type = "ServerReady"_id;
+struct ServerChangeRole
+{
+  static constexpr gf::Id type = "ServerChangeRole"_id;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, ServerReady& data) {
-    return ar;
-}
-
-struct ServerGameStart {
-    static constexpr gf::Id type = "ServerGameStart"_id;
-    GameSettigns settings;
-};
-template<typename Archive>
-  Archive& operator|(Archive& ar, ServerGameStart& data) {
-    return ar | data.settings;
-}
-struct ServerGameOver {
-    static constexpr gf::Id type = "ServerGameOver"_id;
-};
-template<typename Archive>
-  Archive& operator|(Archive& ar, ServerGameOver& data) {
-    return ar;
+template <typename Archive>
+Archive &operator|(Archive &ar, ServerChangeRole &data)
+{
+  return ar;
 }
 
-//Client -> serveur
+// Les autres changement
 
-struct ClientJoinRoom {
-    static constexpr gf::Id type = "ClientJoinRoom"_id;
+struct ServerReady
+{
+  static constexpr gf::Id type = "ServerReady"_id;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, ClientJoinRoom& data) {
-    return ar;
+template <typename Archive>
+Archive &operator|(Archive &ar, ServerReady &data)
+{
+  return ar;
 }
 
-struct ClientLeaveRoom {
-    static constexpr gf::Id type = "ClientLeaveRoom"_id;
+struct ServerGameStart
+{
+  static constexpr gf::Id type = "ServerGameStart"_id;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, ClientLeaveRoom& data) {
-    return ar;
+template <typename Archive>
+Archive &operator|(Archive &ar, ServerGameStart &data)
+{
+  return ar;
+}
+struct ServerGameEnd
+{
+  static constexpr gf::Id type = "ServerGameOver"_id;
+};
+template <typename Archive>
+Archive &operator|(Archive &ar, ServerGameEnd &data)
+{
+  return ar;
 }
 
-struct ClientChangeRole {
-    static constexpr gf::Id type = "ClientChangeRole"_id;
+// Client -> serveur
+
+struct ClientJoinRoom
+{
+  static constexpr gf::Id type = "ClientJoinRoom"_id;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, ClientChangeRole& data) {
-    return ar;
+template <typename Archive>
+Archive &operator|(Archive &ar, ClientJoinRoom &data)
+{
+  return ar;
 }
 
-struct ClientReady {
-    static constexpr gf::Id type = "ClientReady"_id;
+struct ClientLeaveRoom
+{
+  static constexpr gf::Id type = "ClientLeaveRoom"_id;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, ClientReady& data) {
-    return ar;
+template <typename Archive>
+Archive &operator|(Archive &ar, ClientLeaveRoom &data)
+{
+  return ar;
+};
+
+struct ClientChangeRole
+{
+  static constexpr gf::Id type = "ClientChangeRole"_id;
+};
+template <typename Archive>
+Archive &operator|(Archive &ar, ClientChangeRole &data)
+{
+  return ar;
+}
+
+struct ClientReady
+{
+  static constexpr gf::Id type = "ClientReady"_id;
+};
+template <typename Archive>
+Archive &operator|(Archive &ar, ClientReady &data)
+{
+  return ar;
 }
 
 //
 
-struct ClientMove {
-    static constexpr gf::Id type = "ClientMove"_id;
-    char moveDir;
+struct ClientMove
+{
+  static constexpr gf::Id type = "ClientMove"_id;
+  char moveDir;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, ClientMove& data) {
-    return ar | data.moveDir;
+template <typename Archive>
+Archive &operator|(Archive &ar, ClientMove &data)
+{
+  return ar | data.moveDir;
 }
 
-struct CaseCommon {
-    static constexpr gf::Id type = "CaseCommon"_id;
-    CellType celltype;
-    bool pacGomme = false;
-    CaseCommon() : celltype(CellType::Floor) {}
-    CaseCommon(CellType t, bool pg) {celltype = t; pacGomme = pg;}
+struct CaseCommon
+{
+  static constexpr gf::Id type = "CaseCommon"_id;
+  CellType celltype;
+  bool pacGomme = false;
+  CaseCommon() : celltype(CellType::Floor) {}
+  CaseCommon(CellType t, bool pg)
+  {
+    celltype = t;
+    pacGomme = pg;
+  }
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, CaseCommon& data) {
-    return ar | data.celltype | data.pacGomme;
+template <typename Archive>
+Archive &operator|(Archive &ar, CaseCommon &data)
+{
+  return ar | data.celltype | data.pacGomme;
 }
 
-struct BoardCommon {
-    BoardCommon() {};
-    BoardCommon(unsigned int w, unsigned int h) : grid({w, h}), width(w), height(h) {}
-    static constexpr gf::Id type = "BoardCommon"_id;
-    unsigned int width;
-    unsigned int height;
-    gf::Array2D<CaseCommon> grid;
+struct BoardCommon
+{
+  BoardCommon() {};
+  BoardCommon(unsigned int w, unsigned int h) : grid({w, h}), width(w), height(h) {}
+  static constexpr gf::Id type = "BoardCommon"_id;
+  unsigned int width;
+  unsigned int height;
+  gf::Array2D<CaseCommon> grid;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, BoardCommon& data) {
-    return ar | data.width | data.height | data.grid;
+template <typename Archive>
+Archive &operator|(Archive &ar, BoardCommon &data)
+{
+  return ar | data.width | data.height | data.grid;
 }
 
-
-struct GameState {
-    static constexpr gf::Id type = "GameState"_id;
-    std::vector<PlayerData> clientStates;
-    BoardCommon board;
+struct GameState
+{
+  static constexpr gf::Id type = "GameState"_id;
+  std::vector<PlayerData> clientStates;
+  BoardCommon board;
 };
-template<typename Archive>
-  Archive& operator|(Archive& ar, GameState& data) {
-    return ar | data.clientStates | data.board;
+template <typename Archive>
+Archive &operator|(Archive &ar, GameState &data)
+{
+  return ar | data.clientStates | data.board;
 }
-
-
